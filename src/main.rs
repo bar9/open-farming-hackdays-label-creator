27c1c85d4299543eee072a4cb3428bc960cc46c1
rb <r.brand@iwf.ch>
2024-08-16 14:26:15 +0200

    copy to clipboard and sensible high-level state flow

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use std::ops::Deref;
 use serde::{Deserialize, Serialize};
 use serde_qs::to_string as to_query_string;
 use serde_qs::from_str as from_query_string;
+use web_sys::window;
 
 fn main() {
     launch(app);
 }
 
-#[derive(PartialEq, Debug, Serialize, Deserialize)]
+#[derive(PartialEq, Debug, Clone, Serialize, Deserialize)]
 struct AppState {
     username: String,
     password: String
@@ -30,28 +31,35 @@ impl AppState {
     }
     fn to_form_values(&self) -> HashMap<String, FormValue> {
         let mut map = HashMap::new();
-        &map.insert("username".to_string(), FormValue(vec![self.username.clone()]));
-        &map.insert("password".to_string(), FormValue(vec![self.password.clone()]));
+        let _ = &map.insert("username".to_string(), FormValue(vec![self.username.clone()]));
+        let _ = &map.insert("password".to_string(), FormValue(vec![self.password.clone()]));
         map
     }
 
-    fn from_initial_qs() -> AppState {
-        let window = web_sys::window().unwrap();
-        let query_string = window.location().search().unwrap_or_else(|_| "?".to_string());
-        let query_string = &query_string[1..]; // Skip the '?' at the start
-        from_query_string(query_string).unwrap_or_default()
-    }
 }
 
 impl Default for AppState {
     fn default() -> Self {
+        if let Some(window) = web_sys::window() {
+            if let Ok(mut query_string) = window.location().search() {
+                query_string = query_string.trim_start_matches('?').to_string();
+                if let Ok(app_state_from_query_string) = from_query_string::<AppState>(
+                    &query_string
+                ) {
+                    return app_state_from_query_string;
+                }
+            }
+        }
         AppState {username: "admin".to_string(), password: "admin".to_string()}
     }
 }
 
 fn app() -> Element {
-    let mut initial_app_state = AppState::from_initial_qs();
-    let mut form_values = use_signal(|| initial_app_state.to_form_values());
+    let initial_app_state = use_memo(
+        move || AppState::default()
+    );
+
+    let mut form_values = use_signal(|| initial_app_state().to_form_values());
     let app_state = use_memo(
         move || AppState::from_form_values(&form_values.read())
     );
@@ -59,42 +67,41 @@ fn app() -> Element {
         move || to_query_string(app_state.read().deref()).unwrap()
     );
 
-    // let mut should_update_qs = use_signal(|| false);
-    //
-    // let app_state_qs = use_resource(move || async move {
-    //     if *should_update_qs.read() {
-    //         let window = web_sys::window().unwrap();
-    //
-    //         let qs = to_query_string(app_state.read().deref()).unwrap();
-    //         if format!("?{}", qs) != window.location().search().unwrap() {
-    //             window.location().set_search(&qs);
-    //         }
-    //         should_update_qs.set(false);
-    //     }
-    // });
-
     rsx! {
-        form {
-            oninput: move |ev| {
-                form_values.set(ev.values());
-                println!("Input event: {:#?}", ev);
-            },
-            label { r#for: "username", "Username" }
-            input {
-                r#type: "text",
-                name: "username",
-                value: &*app_state.read().username,
+        div {class: "h-screen flex flex-col justify-center items-center gap-3",
+            form {
+                oninput: move |ev| {
+                    form_values.set(ev.values());
+                    println!("Input event: {:#?}", ev);
+                },
+                label { r#for: "username", "Username" }
+                input {
+                    r#type: "text",
+                    name: "username",
+                    value: &*app_state.read().username,
 
+                }
+                label { r#for: "password", "Password" }
+                input {
+                    r#type: "text",
+                    name: "password",
+                    value: &*app_state.read().password,
+                }
             }
-            label { r#for: "password", "Password" }
-            input {
-                r#type: "text",
-                name: "password",
-                value: &*app_state.read().password,
+            pre {"{form_values:#?}"}
+            pre {"{app_state:?}"}
+            pre {"http://localhost:8080/?{app_state_qs}"}
+            button {
+                onclick: move |_: MouseEvent| {
+                    let window = web_sys::window().expect("no global `window` exists");
+                    let navigator = window.navigator();
+                    let clipboard = navigator.clipboard();
+                    let text = format!("http://localhost:8080/?{app_state_qs}");
+                    let  _ = clipboard.write_text(&text);
+                },
+                "copy to clipboard"
             }
+
         }
-        pre {"{form_values:#?}"}
-        pre {"{app_state:?}"}
-        pre {"{app_state_qs}"}
     }
 }
