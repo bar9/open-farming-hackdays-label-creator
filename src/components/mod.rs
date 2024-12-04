f2dfe0f911d57b7b5ffa8c6ea67209ff5ded91bb
bar9 <rolandbrand11@gmail.com>
2024-12-04 20:02:18 +0100

    recipe scale wip

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -253,6 +253,9 @@ pub fn IngredientsTable(mut props: IngredientsTableProps) -> Element {
     let delete_callback = |index, mut list: Signal<Vec<IngredientItem>>| list.remove(index);
     let mut name_to_add = use_signal(|| String::new());
     let mut amount_to_add = use_signal(|| 0);
+    let mut scale_together = use_signal(|| false);
+    let mut amount_to_edit = use_signal(|| 0);
+    let mut key_to_edit = use_signal(|| 0);
     rsx! {
         div { class: "flex flex-col gap-4",
             table { class: "table border-solid",
@@ -266,6 +269,69 @@ pub fn IngredientsTable(mut props: IngredientsTableProps) -> Element {
                             "{ingr.basicInfo.amount} g"
                         }
                         td {
+                            ul {
+                                class: "rounded-box menu",
+                                li {
+                                    details {
+                                        summary {
+                                            "Menge Anpassen"
+                                        }
+                                        ul {
+                                            li {
+                                                div {
+                                                    class: "form-control",
+                                                    label {
+                                                        class: "label cursor-pointer",
+                                                        span {
+                                                            class: "label-text",
+                                                            "Verhältnisse beibehalten"
+                                                        }
+                                                        input {
+                                                            class: "checkbox",
+                                                            r#type: "checkbox",
+                                                            checked: "{scale_together}",
+                                                            oninput: move |e| scale_together.set(e.value() == "true")
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                            li {
+                                                input {
+                                                    r#type: "number",
+                                                    placeholder: "Menge",
+                                                    class: "input input-bordered bg-white input-accent w-full",
+                                                    onchange: move |evt| {
+                                                        if let Ok(amount) = evt.data.value().parse::<i32>() {
+                                                            amount_to_edit.set(amount);
+                                                        }
+                                                    },
+                                                    value: "{ingr.basicInfo.amount}"
+                                                }
+                                                button { class: "btn btn-accent",
+                                                    onclick: move |_evt|  {
+                                                        if *scale_together.read() {
+                                                            // let factor = (&*amount_to_edit)() / props.ingredients.read().get(key).unwrap().basicInfo.amount;
+                                                            let factor = &*amount_to_edit;
+                                                            let factor = 5;
+                                                            let ingredients = props.ingredients.read().clone();
+                                                            for (key, elem) in ingredients.iter().enumerate() {
+                                                                let name = elem.basicInfo.standard_ingredient.name.clone();
+                                                                props.ingredients.write()[key] = IngredientItem::from_name_amount( name, elem.basicInfo.amount * factor);
+                                                            }
+                                                        } else {
+                                                            let name = (props.ingredients.read().get(key).unwrap().basicInfo.standard_ingredient.name.clone());
+                                                            props.ingredients.write()[key] = IngredientItem::from_name_amount( name, (&*amount_to_edit)() );
+                                                        }
+                                                    },
+                                                    "Anpassen"
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        td {
                             button {
                                 class: "btn btn-square",
                                 dangerous_inner_html: r###"<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>"###,
@@ -273,6 +339,7 @@ pub fn IngredientsTable(mut props: IngredientsTableProps) -> Element {
                                     delete_callback(key, props.ingredients.clone());
                                 }
                             }
+
                         }
                     }
                 }
