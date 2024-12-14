bd53b77b19c5b059ad5417d9e9dc727ae67805dc
bar9 <rolandbrand11@gmail.com>
2024-12-14 09:58:46 +0100

    wasm-bindgen fix

--- src/components/ingredients_table.rs
+++ src/components/ingredients_table.rs
@@ -4,6 +4,8 @@ use crate::model::{food_db, IngredientItem};
 #[derive(Props, Clone, PartialEq)]
 pub struct IngredientsTableProps {
     ingredients: Signal<Vec<IngredientItem>>
+    // TODO: accept validation messages
+    // TODO: accept
 }
 pub fn IngredientsTable(mut props: IngredientsTableProps) -> Element {
     let delete_callback = |index, mut list: Signal<Vec<IngredientItem>>| list.remove(index);
@@ -25,6 +27,7 @@ pub fn IngredientsTable(mut props: IngredientsTableProps) -> Element {
                             "{ingr.basicInfo.amount} g"
                         }
                         td {
+                            IngredientDetail {}
                             ul {
                                 class: "rounded-box menu",
                                 li {
@@ -137,5 +140,37 @@ pub fn IngredientsTable(mut props: IngredientsTableProps) -> Element {
                 "Hinzufügen"
             }
         }
+    }
+}
+
+#[derive(Props, Clone, PartialEq)]
+pub struct IngredientDetailProps {
+
+}
+pub fn IngredientDetail(props: IngredientDetailProps) -> Element {
+    let mut is_open = use_signal(|| false);
+    rsx! {
+        button {
+            class: "btn btn-square",
+            onclick: move |_| is_open.toggle(),
+            dangerous_inner_html: r###"<svg class="h-6 w-6" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>"###,
+        }
+        dialog {
+                open: "{is_open}",
+                class: "modal",
+                div {
+                    class: "modal-box bg-base-100",
+                    h3 { class:"font-bold text-lg", "TODO_LABEL" }
+                    pre {"Ingredient Detail"}
+                    div {class: "modal-action",
+                        form {method: "dialog",
+                            button {class:"btn btn-sm",
+                                onclick: move |_| is_open.toggle(),
+                                "× Schliessen"
+                            }
+                        }
+                    }
+                }
+            }
     }
 }
