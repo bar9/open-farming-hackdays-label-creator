8a7846d3616dbd578131b5dd205a775d22eb8f7c
bar9 <rolandbrand11@gmail.com>
2025-07-17 12:00:04 +0200

    add missing file

---
+++ src/shared.rs
@@ -0,0 +1,13 @@
+use dioxus::prelude::*;
+use std::collections::HashMap;
+
+#[derive(Clone, Copy)]
+pub struct Validations(pub Memo<HashMap<String, &'static str>>);
+
+#[derive(Clone, Copy)]
+pub struct Conditionals(pub Memo<HashMap<String, bool>>);
+
+#[derive(Clone, Copy)]
+pub enum Configuration {
+    Conventional
+}
