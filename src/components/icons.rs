10711be18d6130f7985ee6d7c85dd1aecb830da8
bar9 <rolandbrand11@gmail.com>
2026-09-09 10:33:23 +0200

    chore(icons): drei nie eingebundene Icons entfernt
    InstanceSelect, Settings und Database wurden ausserhalb von icons.rs
    nirgends referenziert -- weder in rsx noch in Tests.

--- src/components/icons.rs
+++ src/components/icons.rs
@@ -88,48 +88,7 @@ pub fn Clipboard() -> Element {
     }
 }
 
-#[component]
-pub fn InstanceSelect() -> Element {
-    rsx! {
-        svg {
-            class: "w-6 h-6",
-            "stroke-linejoin": "round",
-            "viewBox": "0 0 24 24",
-            "stroke-width": "2",
-            fill: "none",
-            width: "24",
-            "stroke-linecap": "round",
-            stroke: "currentColor",
-            xmlns: "http://www.w3.org/2000/svg",
-            height: "24",
-            path { stroke: "none", d: "M0 0h24v24H0z", fill: "none" }
-            path { d: "M3 5m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" }
-            path { d: "M6 8h.01" }
-            path { d: "M9 8h.01" }
-        }
-    }
-}
 
-#[component]
-pub fn Settings() -> Element {
-    rsx! {
-        svg {
-            class: "w-6 h-6",
-            width: "24",
-            "stroke-width": "2",
-            fill: "none",
-            "viewBox": "0 0 24 24",
-            stroke: "currentColor",
-            "stroke-linejoin": "round",
-            "stroke-linecap": "round",
-            height: "24",
-            xmlns: "http://www.w3.org/2000/svg",
-            path { fill: "none", d: "M0 0h24v24H0z", stroke: "none" }
-            path { d: "M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" }
-            path { d: "M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" }
-        }
-    }
-}
 
 #[component]
 pub fn DashedPlus() -> Element {
@@ -188,27 +147,6 @@ pub fn DashedX() -> Element {
     }
 }
 
-#[component]
-pub fn Database() -> Element {
-    rsx! {
-        svg {
-            class: "w-3 h-3 inline-block mr-1",
-            fill: "none",
-            height: "16",
-            stroke: "currentColor",
-            stroke_linecap: "round",
-            stroke_linejoin: "round",
-            stroke_width: "2",
-            view_box: "0 0 24 24",
-            width: "16",
-            xmlns: "http://www.w3.org/2000/svg",
-            path { d: "M0 0h24v24H0z", fill: "none", stroke: "none" }
-            ellipse { cx: "12", cy: "5", rx: "9", ry: "3" }
-            path { d: "M21 12c0 1.66-4 3-9 3s-9-1.34-9-3" }
-            path { d: "M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5" }
-        }
-    }
-}
 
 #[component]
 pub fn BioSuisseRegular() -> Element {
